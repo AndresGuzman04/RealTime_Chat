22f7b8c2fc673b1cab36c39ea93ebff2dace5058
AndresGuzman04 <andr35guzman2001@gmail.com>
2025-01-01 22:11:25 -0600

    Add connection for databases

--- server/index.js
+++ server/index.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import logger from 'morgan'
+import dotenv from 'dotenv';
+import {createClient} from '@libsql/client'; 
 
 import { Server } from 'socket.io';
 import { createServer } from 'node:http';
+
+dotenv.config()
 
 const port = process.env.PORT ?? 3000;
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {}
+});
+
+const db = createClient({
+  url: process.env.URL,
+  authToken: process.env.DB_TOKEN
+})
+
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT 
+  )
+  `)
 
 io.on('connection',(socket) => {
   console.log('a client connected');
@@ -17,8 +35,20 @@ io.on('connection',(socket) => {
     console.log('a client disconnected');
   });
 
-  socket.on('chat message', (msg) =>{
-    io.emit('chat message', msg);
+  socket.on('chat message', async (msg) =>{
+    let result
+
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES (:msg)',
+        args: {msg}
+      })
+    } catch (error) {
+      console.error(error)
+      return
+    }
+
+    io.emit('chat message', msg, result.lastInsertRowid.toString());
   })
 })
 
